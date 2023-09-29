c32c915e4b8616c0af28a6932b887eeb8962fced
Christian Johansen <christian.johansen@mattilsynet.no>
2023-09-29 22:49:44 +0200

    En blogg blir til: Litt tekst, litt system og bittelitt styling

---
+++ src/parenteser/core.clj
@@ -0,0 +1,85 @@
+(ns parenteser.core
+  (:require [parenteser.ingest :as ingest]
+            [parenteser.pages :as pages]
+            [powerpack.app :as app]
+            [powerpack.export :as export]
+            [powerpack.highlight :as highlight]))
+
+(def config
+  {:site/base-url "https://parenteser.mattilsynet.io"
+   :site/default-language "no"
+   :site/title "Parenteser"
+
+   :stasis/build-dir "build"
+   :powerpack/content-dir "resources"
+   :powerpack/source-dirs ["src" "dev"]
+   :powerpack/db "datomic:mem://parenteser"
+
+   :optimus/assets [{:public-dir "public"
+                     :paths [#"/images/*.*"]}]
+   :optimus/bundles {"styles.css"
+                     {:public-dir "public"
+                      :paths ["/css/parenteser.css"
+                              "/css/pygments.css"]}}
+
+   :powerpack.server/port 5052
+
+   :imagine/config {:prefix "image-assets"
+                    :resource-path "public"
+                    :disk-cache? true
+                    :transformations
+                    {:vcard-small
+                     {:transformations [[:fit {:width 184 :height 184}]
+                                        [:crop {:preset :square}]]
+                      :retina-optimized? true
+                      :retina-quality 0.4
+                      :width 184}}}
+
+   :datomic/schema [{:db/ident :page/king
+                     :db/valueType :db.type/keyword
+                     :db/unique :db.unique/identity
+                     :db/cardinality :db.cardinality/one}
+
+                    {:db/ident :tag/id
+                     :db/valueType :db.type/keyword
+                     :db/unique :db.unique/identity
+                     :db/cardinality :db.cardinality/one}
+
+                    {:db/ident :tag/name
+                     :db/valueType :db.type/string
+                     :db/cardinality :db.cardinality/one}
+
+                    {:db/ident :blog-post/title
+                     :db/valueType :db.type/string
+                     :db/cardinality :db.cardinality/one}
+
+                    {:db/ident :blog-post/description
+                     :db/valueType :db.type/string
+                     :db/cardinality :db.cardinality/one}
+
+                    {:db/ident :blog-post/published
+                     :db/valueType :db.type/instant
+                     :db/cardinality :db.cardinality/one}
+
+                    {:db/ident :blog-post/updated
+                     :db/valueType :db.type/instant
+                     :db/cardinality :db.cardinality/one}
+
+                    {:db/ident :blog-post/tags
+                     :db/valueType :db.type/ref
+                     :db/cardinality :db.cardinality/many}
+
+                    {:db/ident :blog-post/image
+                     :db/valueType :db.type/ref
+                     :db/cardinality :db.cardinality/one}
+
+                    {:db/ident :blog-post/body
+                     :db/valueType :db.type/string
+                     :db/cardinality :db.cardinality/one}]})
+
+(defn create-app []
+  (-> {:config config
+       :create-ingest-tx #'ingest/create-tx
+       :render-page #'pages/render-page}
+      app/create-app
+      highlight/install))
---
+++ src/parenteser/pages.clj
@@ -0,0 +1,48 @@
+(ns parenteser.pages
+  (:require [powerpack.html :as html]
+            [powerpack.markdown :as md]
+            [imagine.core :as imagine]
+            [datomic-type-extensions.api :as d]))
+
+(defn get-blog-posts [db]
+  (->> (d/q '[:find [?e ...]
+              :where
+              [?e :page/uri]
+              [?e :page/kind :page.kind/blog-post]]
+            db)
+       (map #(d/entity db %))))
+
+(defn render-frontpage [req page]
+  (html/render-hiccup
+   req
+   page
+   [:div
+    [:h1 "Parenteser"]
+    [:p "Beretninger fra Mat-teamets grønne enger"]
+    [:ul
+     (for [blog-post (get-blog-posts (d/entity-db page))]
+       [:li [:a {:href (:page/uri blog-post)}
+             (:page/title blog-post)]])]]))
+
+(defn render-blog-post [req blog-post]
+  (html/render-hiccup
+   req
+   blog-post
+   [:div
+    [:h1 (:page/title blog-post)]
+    (md/to-html (:blog-post/body blog-post))
+    [:img {:src "/vcard-small/images/christian.jpg"}]]))
+
+(defn render-404 [req page]
+  (html/render-hiccup
+   req
+   page
+   [:h1 "404 WAT"]))
+
+(defn render-page [req page]
+  (if-let [f (case (:page/kind page)
+               :page.kind/frontpage render-frontpage
+               :page.kind/blog-post render-blog-post
+               nil)]
+    (f req page)
+    (render-404 req page)))
