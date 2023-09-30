994e79102e5434c78a096799b5ddc996eaabe667
Christian Johansen <christian.johansen@mattilsynet.no>
2023-09-30 13:06:50 +0200

    Færre defs gir mer REPL reload-glede

--- src/parenteser/core.clj
+++ src/parenteser/core.clj
@@ -5,102 +5,100 @@
             [powerpack.export :as export]
             [powerpack.highlight :as highlight]))
 
-(def config
-  {:site/base-url "https://parenteser.mattilsynet.io"
-   :site/default-language "no"
-   :site/title "Parenteser"
-
-   :stasis/build-dir "build"
-   :powerpack/content-dir "resources"
-   :powerpack/source-dirs ["src" "dev"]
-   :powerpack/db "datomic:mem://parenteser"
-
-   :optimus/assets [{:public-dir "public"
-                     :paths [#"/images/*.*"
-                             #"/fonts/*"]}]
-   :optimus/bundles {"styles.css"
-                     {:public-dir "public"
-                      :paths ["/css/parenteser.css"]}}
-
-   :powerpack.server/port 5052
-
-   :imagine/config {:prefix "image-assets"
-                    :resource-path "public"
-                    :disk-cache? true
-                    :transformations
-                    {:vcard-small
-                     {:transformations [[:fit {:width 184 :height 184}]
-                                        [:crop {:preset :square}]]
-                      :retina-optimized? true
-                      :retina-quality 0.4
-                      :width 184}}}
-
-   :datomic/schema [{:db/ident :page/king
-                     :db/valueType :db.type/keyword
-                     :db/unique :db.unique/identity
-                     :db/cardinality :db.cardinality/one}
-
-                    {:db/ident :tag/id
-                     :db/valueType :db.type/keyword
-                     :db/unique :db.unique/identity
-                     :db/cardinality :db.cardinality/one}
-
-                    {:db/ident :tag/name
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one}
-
-                    {:db/ident :blog-post/title
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one}
-
-                    {:db/ident :blog-post/description
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one}
-
-                    {:db/ident :blog-post/published
-                     :db/valueType :db.type/instant
-                     :db/cardinality :db.cardinality/one}
-
-                    {:db/ident :blog-post/updated
-                     :db/valueType :db.type/instant
-                     :db/cardinality :db.cardinality/one}
-
-                    {:db/ident :blog-post/tags
-                     :db/valueType :db.type/ref
-                     :db/cardinality :db.cardinality/many}
-
-                    {:db/ident :blog-post/image
-                     :db/valueType :db.type/ref
-                     :db/cardinality :db.cardinality/one}
-
-                    {:db/ident :blog-post/body
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one}
-
-                    {:db/ident :blog-post/author
-                     :db/valueType :db.type/ref
-                     :db/cardinality :db.cardinality/one}
-
-                    {:db/ident :person/id
-                     :db/valueType :db.type/keyword
-                     :db/cardinality :db.cardinality/one
-                     :db/unique :db.unique/identity}
-
-                    {:db/ident :person/given-name
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one}
-
-                    {:db/ident :person/family-name
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one}
-
-                    ;; Bytt ut med rullerende bilder etterhvert
-                    {:db/ident :person/photo
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one}]})
-
 (defn create-app []
-  (-> {:config config
+  (-> {:config
+       {:site/base-url "https://parenteser.mattilsynet.io"
+        :site/default-language "no"
+        :site/title "Parenteser"
+
+        :stasis/build-dir "build"
+        :powerpack/content-dir "resources"
+        :powerpack/source-dirs ["src" "dev"]
+        :powerpack/db "datomic:mem://parenteser"
+
+        :optimus/assets [{:public-dir "public"
+                          :paths [#"/images/*.*"
+                                  #"/fonts/*"]}]
+        :optimus/bundles {"styles.css"
+                          {:public-dir "public"
+                           :paths ["/css/parenteser.css"]}}
+
+        :powerpack.server/port 5052
+
+        :imagine/config {:prefix "image-assets"
+                         :resource-path "public"
+                         :disk-cache? true
+                         :transformations
+                         {:vcard-small
+                          {:transformations [[:fit {:width 184 :height 184}]
+                                             [:crop {:preset :square}]]
+                           :retina-optimized? true
+                           :retina-quality 0.4
+                           :width 184}}}
+
+        :datomic/schema [{:db/ident :page/kind
+                          :db/valueType :db.type/keyword
+                          :db/unique :db.unique/identity
+                          :db/cardinality :db.cardinality/one}
+
+                         {:db/ident :tag/id
+                          :db/valueType :db.type/keyword
+                          :db/unique :db.unique/identity
+                          :db/cardinality :db.cardinality/one}
+
+                         {:db/ident :tag/name
+                          :db/valueType :db.type/string
+                          :db/cardinality :db.cardinality/one}
+
+                         {:db/ident :blog-post/title
+                          :db/valueType :db.type/string
+                          :db/cardinality :db.cardinality/one}
+
+                         {:db/ident :blog-post/description
+                          :db/valueType :db.type/string
+                          :db/cardinality :db.cardinality/one}
+
+                         {:db/ident :blog-post/published
+                          :db/valueType :db.type/instant
+                          :db/cardinality :db.cardinality/one}
+
+                         {:db/ident :blog-post/updated
+                          :db/valueType :db.type/instant
+                          :db/cardinality :db.cardinality/one}
+
+                         {:db/ident :blog-post/tags
+                          :db/valueType :db.type/ref
+                          :db/cardinality :db.cardinality/many}
+
+                         {:db/ident :blog-post/image
+                          :db/valueType :db.type/ref
+                          :db/cardinality :db.cardinality/one}
+
+                         {:db/ident :blog-post/body
+                          :db/valueType :db.type/string
+                          :db/cardinality :db.cardinality/one}
+
+                         {:db/ident :blog-post/author
+                          :db/valueType :db.type/ref
+                          :db/cardinality :db.cardinality/one}
+
+                         {:db/ident :person/id
+                          :db/valueType :db.type/keyword
+                          :db/cardinality :db.cardinality/one
+                          :db/unique :db.unique/identity}
+
+                         {:db/ident :person/given-name
+                          :db/valueType :db.type/string
+                          :db/cardinality :db.cardinality/one}
+
+                         {:db/ident :person/family-name
+                          :db/valueType :db.type/string
+                          :db/cardinality :db.cardinality/one}
+
+                         ;; Bytt ut med rullerende bilder etterhvert
+                         {:db/ident :person/photo
+                          :db/valueType :db.type/string
+                          :db/cardinality :db.cardinality/one}]}
        :create-ingest-tx #'ingest/create-tx
        :render-page #'pages/render-page}
       app/create-app
