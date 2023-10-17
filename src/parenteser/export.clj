fefabe5e874456772325767a2cbfb225a4bcfc30
Christian Johansen <christian.johansen@mattilsynet.no>
2023-10-17 12:54:11 +0200

    Gjør klart for eksport/bygg

---
+++ src/parenteser/export.clj
@@ -0,0 +1,6 @@
+(ns parenteser.export
+  (:require [parenteser.core :as parenteser]
+            [powerpack.export :as export]))
+
+(defn ^:export export [& _args]
+  (export/export (parenteser/create-app) {:format :json}))
