c25e1fe95dba95931cd9687c82328faeb3a078ee
Christian Johansen <christian.johansen@mattilsynet.no>
2023-11-06 16:52:02 +0100

    Oppgrader Powerpack

--- dev/parenteser/dev.clj
+++ dev/parenteser/dev.clj
@@ -1,20 +1,19 @@
 (ns parenteser.dev
   (:require [integrant.core :as ig]
             [parenteser.core :as parenteser]
-            [powerpack.app :as app]
+            [powerpack.dev :as dev]
             [powerpack.export :as export]))
 
-(defmethod ig/init-key :powerpack/app [_ _]
-  (-> (parenteser/create-app)
-      (assoc-in [:config :site/base-url] nil)))
+(defmethod ig/init-key :powerpack/powerpack [_ _]
+  (parenteser/create-app))
 
 (comment
 
   (set! *print-namespace-maps* false)
 
-  (app/start)
-  (app/stop)
-  (app/reset)
+  (dev/start)
+  (dev/stop)
+  (dev/reset)
 
   (export/export (parenteser/create-app))
 
--- src/parenteser/core.clj
+++ src/parenteser/core.clj
@@ -4,46 +4,45 @@
             [powerpack.highlight :as highlight]))
 
 (defn create-app []
-  (-> {:config
-       {:site/base-url "https://parenteser.mattilsynet.io"
-        :site/default-locale :no
-        :site/title "Parenteser"
+  (-> {:site/base-url "https://parenteser.mattilsynet.io"
+       :site/default-locale :no
+       :site/title "Parenteser"
 
-        :stasis/build-dir "docker/build"
-        :powerpack/content-dir "content"
-        :powerpack/source-dirs ["src" "dev"]
-        :powerpack/resource-dirs ["resources"]
-        :powerpack/db "datomic:mem://parenteser"
+       :powerpack/build-dir "docker/build"
+       :powerpack/content-dir "content"
+       :powerpack/source-dirs ["src" "dev"]
+       :powerpack/resource-dirs ["resources"]
+       :datomic/uri "datomic:mem://parenteser"
+       :datomic/schema-file "resources/schema.edn"
 
-        :optimus/assets [{:public-dir "public"
-                          :paths [#"/*.(png|ico)"
-                                  #"/images/*.*"
-                                  #"/fonts/*"]}]
-        :optimus/bundles {"styles.css"
-                          {:public-dir "public"
-                           :paths ["/css/parenteser.css"]}}
+       :optimus/assets [{:public-dir "public"
+                         :paths [#"/*.(png|ico)"
+                                 #"/images/*.*"
+                                 #"/fonts/*"]}]
+       :optimus/bundles {"styles.css"
+                         {:public-dir "public"
+                          :paths ["/css/parenteser.css"]}}
 
-        :powerpack/port 5052
+       :powerpack/port 5052
 
-        :imagine/config {:prefix "image-assets"
-                         :resource-path "public"
-                         :disk-cache? true
-                         :transformations
-                         {:vcard-small
-                          {:transformations [[:fit {:width 184 :height 184}]
-                                             [:crop {:preset :square}]]
-                           :retina-optimized? true
-                           :retina-quality 0.4
-                           :width 184}
+       :imagine/config {:prefix "image-assets"
+                        :resource-path "public"
+                        :disk-cache? true
+                        :transformations
+                        {:vcard-small
+                         {:transformations [[:fit {:width 184 :height 184}]
+                                            [:crop {:preset :square}]]
+                          :retina-optimized? true
+                          :retina-quality 0.4
+                          :width 184}
 
-                          :vcard-open-graph
-                          {:transformations [[:fit {:width 92 :height 92}]
-                                             [:crop {:preset :square}]]
-                           :retina-optimized? false
-                           :width 92}}}
+                         :vcard-open-graph
+                         {:transformations [[:fit {:width 92 :height 92}]
+                                            [:crop {:preset :square}]]
+                          :retina-optimized? false
+                          :width 92}}}
 
-        :datomic/schema-file "resources/schema.edn"}
-       :create-ingest-tx #'ingest/create-tx
-       :render-page #'pages/render-page
-       :on-ingested #'ingest/on-ingested}
+       :powerpack/create-ingest-tx #'ingest/create-tx
+       :powerpack/render-page #'pages/render-page
+       :powerpack/on-ingested #'ingest/on-ingested}
       highlight/install))
