ccdf62945c78262520f82b70d2c3493bd3b46b24
Christian Johansen <christian.johansen@mattilsynet.no>
2024-06-03 08:10:00 +0200

    Lag Portfolio-scene for teaser section

--- portfolio/parenteser/portfolio.cljs
+++ portfolio/parenteser/portfolio.cljs
@@ -1,9 +1,11 @@
 (ns parenteser.portfolio
   (:require [portfolio.ui :as ui]
             parenteser.scenes.teaser-scenes
+            parenteser.scenes.teaser-section-scenes
             parenteser.scenes.vcard-scenes))
 
 :parenteser.scenes.teaser-scenes/keep
+:parenteser.scenes.teaser-section-scenes/keep
 :parenteser.scenes.vcard-scenes/keep
 
 (ui/start!
---
+++ portfolio/parenteser/scenes/teaser_section_scenes.cljs
@@ -0,0 +1,27 @@
+(ns parenteser.scenes.teaser-section-scenes
+  (:require [parenteser.elements :as e]
+            [portfolio.dumdom :refer-macros [defscene]]))
+
+(defscene default
+  (e/teaser-section
+   {:teasers
+    [{:url "/somewhere/"
+      :title "Certainly an interesting teaser"
+      :description "Interesting things have happenened, and this article is about to
+                  tell you all about it. You won't believe what you're about to read
+                  but I can assure you that it is all true."
+      :published "June 4th 2024"
+      :aside {:title "Homer J. Simpson"
+              :body "About News and Stuff"
+              :image "/portfolio-images/homer.jpg"
+              :image-alt "Homer Simpson"}}
+     {:url "/somewhere/"
+      :title "Clowning is on the rise"
+      :description "Clowns have been repressed much too long, and now we're on
+      the rise world-wide. Read more about what clowning is going on in your
+      area."
+      :published "May 30th 2024"
+      :aside {:title "Herschel Shmoikel Krustofsky"
+              :body "About News and Stuff"
+              :image "/portfolio-images/krusty.jpg"
+              :image-alt "Herschel Shmoikel Krustofsky"}}]}))
